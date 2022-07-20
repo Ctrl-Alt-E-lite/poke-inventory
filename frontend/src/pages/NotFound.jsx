4dd4df03935ee217b4ff5c750c871d56741b4b79
Haley T <haleyqtobias@gmail.com>
2022-07-20 13:07:42 -0700

    BaseCard Update
    ^

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import LinkButton from '../components/LinkButton'
 function NotFound() {
   return (
     <div className='container'>
-      <div className='about-page'>
+      <div className='page'>
           <h1>Pokemon not found: Error 404</h1>
           <p>The pokemon you're trying to find does not exist.</p>
           <LinkButton to={'/'} text={'Home'}/>
