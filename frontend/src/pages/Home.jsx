a537a07a671dfafa8215d670793f72529eac1113
Andrea <andrea.berrocal95@gmail.com>
2022-07-22 10:53:52 -0700

    completed home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,14 @@ function Home() {
   return (
     <div className='container'>
         <div className='home'>
-            <h3>Create Your Own Pokemon Team!</h3>
+          <div className='home-instructions'>
+          <h3>Create Your Own Pokemon Team!</h3>
+            <ul className='instructions'>
+              <li>To view all Pokemon go ahead and visit <a href='/pokedex'>Pokedex</a>, from there you can click on any Pokemon and view further information on them</li>
+              <li>If you would like to view all the current teams, go ahead and click on <a href='/teams'>Team Gallery</a></li>
+              <li>To get started and build your very own Team, click on <a href='/create/team'>Create Team</a>!!!</li>
+            </ul>
+          </div>
         </div>
 
     </div>
